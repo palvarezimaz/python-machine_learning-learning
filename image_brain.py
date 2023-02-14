a5cd4e06e5ded28f079b1d032045dc77343431a3
palvarzimaz <palvarezimaz@gmail.com>
2023-02-15 10:31:42 +1100

    first commit

---
+++ image_brain.py
@@ -0,0 +1,16 @@
+from imageai.Classification import ImageClassification
+import os
+
+execution_path = os.getcwd()
+
+prediction = ImageClassification()
+
+prediction.setModelTypeAsMobileNetV2()
+prediction.setModelPath(os.path.join(
+    execution_path, "mobilenet_v2-b0353104.pth"))
+prediction.loadModel()
+
+predictions, probabilities = prediction.classifyImage(
+    os.path.join(execution_path, 'images/cart2.jpg'), result_count=5)
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(f'{eachPrediction} : {eachProbability}')
